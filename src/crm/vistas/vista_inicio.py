aa4295ea4cb9d9671451b72cda8ec6dd4afb5fc5
Daniel Sepúlveda <dansepdev@gmail.com>
2026-02-08 23:17:00 -0300

    grid() changed for pack()

--- src/crm/vistas/vista_inicio.py
+++ src/crm/vistas/vista_inicio.py
@@ -18,37 +18,34 @@ class VistaInicio(ttk.Frame):
     def __init__(self, padre, app: "App"):
         super().__init__(padre)
         self._app = app
-        self._logo = tk.PhotoImage(file=LOGO_PATH).subsample(4)
+        self._logo = tk.PhotoImage(file=LOGO_PATH)
 
-        # Configuración de columnas y filas "fantasmas" para centrar contenido
-        self.rowconfigure(0, weight=1)
-        self.rowconfigure(6, weight=1)
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(2, weight=1)
+        contenedor = ttk.Frame(self)
+        contenedor.pack(expand=True)
 
         # ----------------------- Elementos de la vista -----------------------
-        imagen = ttk.Label(self, image=self._logo, anchor="center")
-        imagen.grid(column=1, row=1)
+        ttk.Label(contenedor, image=self._logo).pack()
 
         ttk.Label(
-            self,
+            contenedor,
             text="Sistema de Gestión de Clientes",
             font=("TkDefaultFont", 16, "bold"),
-        ).grid(column=1, row=2, pady=20)
+        ).pack(pady=20)
 
-        sub = ttk.Label(self, text="Seleccione una opción para continuar")
-        sub.grid(column=1, row=3, pady=10)
+        ttk.Label(
+            contenedor, text="Seleccione una opción para continuar"
+        ).pack(pady=10)
 
         ttk.Button(
-            self,
+            contenedor,
             text="Gestión de Clientes",
             width=20,
             command=lambda: self._app._mostrar_vista("VistaClientes"),
-        ).grid(column=1, row=4, pady=5)
+        ).pack(pady=5)
 
         ttk.Button(
-            self,
+            contenedor,
             text="Ver registro de LOGS",
             width=20,
             command=lambda: self._app._mostrar_vista("VistaLogs"),
-        ).grid(column=1, row=5, pady=5)
+        ).pack(pady=5)
