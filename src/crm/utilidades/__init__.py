dacf2fea985f662f11ddceb31be045443a5ec0b7
Daniel Sepúlveda <dansepdev@gmail.com>
2026-02-09 18:57:47 -0300

    refactor: UI moved to utilidades folder

--- src/crm/utilidades/__init__.py
+++ src/crm/utilidades/__init__.py
@@ -1,3 +1,4 @@
 # ruff: noqa: F401
 # type: ignore
+from .componentes_tkinter import ComponentesTkinter
 from .gestor_archivos import GestorArchivos
--- src/crm/utilidades/componentes_tkinter.py
+++ src/crm/utilidades/componentes_tkinter.py
@@ -1,16 +1,16 @@
 from tkinter import ttk
 
 
-class UI:
+class ComponentesTkinter:
     @staticmethod
-    def campo(frame, label, columna, fila, mb: bool = False):
+    def campo(frame, label, columna, fila, mb: bool = False, **kwargs):
         cframe = ttk.Frame(frame)
         cframe.grid(column=columna, row=fila)
 
         label_campo = ttk.Label(cframe, text=label, font=("Arial", 14, "bold"))
         label_campo.grid(column=0, row=0, padx=5, sticky="w")
 
-        entry = ttk.Entry(cframe, width=25)
+        entry = ttk.Entry(cframe, width=25, **kwargs)
         entry.grid(column=0, row=1, padx=5, pady=(0, 20) if mb else 0)
         return entry
 
@@ -26,13 +26,11 @@ class UI:
 
         state = "readonly"
         entry = ttk.Combobox(
-            cframe, values=valores, state=state, width=23, **kwargs
+            cframe, values=valores, state=state, width=23, height=8, **kwargs
         )
         entry.grid(column=0, row=1, padx=5, pady=(0, 20) if mb else 0)
         return entry
 
     @staticmethod
-    def _separador(frame, row):
-        ttk.Separator(frame, orient="horizontal").grid(
-            column=0, row=row, pady=10
-        )
+    def separador_horizontal(frame, alto: int = 10):
+        ttk.Separator(frame, orient="horizontal").pack(pady=alto)
