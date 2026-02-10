72ab0da6a298f25dc01e50887b38c29e53910a35
Daniel Sepúlveda <dansepdev@gmail.com>
2026-02-10 08:17:38 -0300

    refactor: ui updated

--- src/crm/vistas/vista_formulario.py
+++ src/crm/vistas/vista_formulario.py
@@ -1,75 +1,165 @@
+import platform
+import tkinter as tk
 from tkinter import ttk
+from typing import TYPE_CHECKING
 
 from src.crm.cliente import Direccion
+from src.crm.utilidades import ComponentesTkinter as Ctk
 
-from .ui import UI
+
+# Se importa App solo en desarrollo para poder usar su tipo. En tiempo de
+# ejecución se ignora el import ya que genera dependencia circular
+if TYPE_CHECKING:
+    from src.crm.app import App
 
 
 class VistaFormulario(ttk.Frame):
-    def __init__(self, padre, app):
+    def __init__(self, padre, app: "App"):
         super().__init__(padre)
-        self.app = app
+        self._app = app
+        self._campos = {}
         regiones = Direccion.obtener_regiones()
 
-        # INFORMACIÓN PERSONAL
-        fr_info = ttk.LabelFrame(self, text="Información Personal", padding=20)
-        fr_info.grid(column=0, row=0)
-        self.entry_rut = UI.campo(fr_info, "RUT", 0, 0, True)
-        self.entry_nombres = UI.campo(fr_info, "Nombres", 1, 0, True)
-        self.entry_paterno = UI.campo(fr_info, "Apellido Paterno", 0, 1)
-        self.entry_materno = UI.campo(fr_info, "Apellido Materno", 1, 1)
-        UI._separador(self, row=1)
+        # Configuración de filas y columnas para centrar el formulario
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=2)
+        self.columnconfigure(2, weight=0)
+        self.columnconfigure(3, weight=1)
+        self.rowconfigure(0, weight=1)
 
-        # DATOS DE CONTACTO
-        fr_contacto = ttk.LabelFrame(self, text="Datos Contacto", padding=20)
-        fr_contacto.grid(column=0, row=2)
-        self.entry_correo = UI.campo(fr_contacto, "Correo", 0, 0)
-        self.entry_telefono = UI.campo(fr_contacto, "Teléfono", 1, 0)
-        UI._separador(self, row=3)
+        # Configuración de canvas (especie de frame que permite scroll)
+        self.canvas = tk.Canvas(self, highlightthickness=0)
+        self.canvas.grid(column=1, row=0, sticky="nsew")
 
-        # DIRECCION
-        fr_dir = ttk.LabelFrame(self, text="Dirección", padding=20)
-        fr_dir.grid(column=0, row=4)
-        self.entry_calle = UI.campo(fr_dir, "Calle", 0, 0, True)
-        self.entry_numero = UI.campo(fr_dir, "Número", 1, 0, True)
-        self.entry_region = UI.combo(fr_dir, "Región", 0, 1, regiones)
-        self.entry_comuna = UI.combo(fr_dir, "Comuna", 1, 1, [])
-        UI._separador(self, row=5)
+        scrollbar = ttk.Scrollbar(self, command=self.canvas.yview)
+        scrollbar.grid(column=2, row=0, sticky="ns")
 
-        self.entry_region.bind("<<ComboboxSelected>>", self.actualizar_comunas)
+        self.canvas.configure(yscrollcommand=scrollbar.set)
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
-        self.label_estado = ttk.Label(self, text="")
-        self.label_estado.grid(column=0, row=6)
+        frame_form = ttk.Frame(self.canvas, padding=(0, 0, 0, 40))
+        self.canvas.create_window((0, 0), window=frame_form, anchor="nw")
 
-        boton = ttk.Button(
-            self, text="Crear usuario", command=self.handle_crear_usuario
+        # 1) SECCIÓN PARA TIPO DE CLIENTE Y SU ATRIBUTO DIFERENCIADOR
+        self.fr_atributos = ttk.LabelFrame(
+            frame_form, text="Cliente", padding=20
         )
-        boton.grid(column=0, row=7)
+        self.fr_atributos.pack()
 
-    def actualizar_comunas(self, _):
-        region = self.entry_region.get()
+        self._campos["tipo"] = Ctk.combo(
+            self.fr_atributos,
+            "Tipo de cliente",
+            columna=0,
+            fila=0,
+            valores=["Regular", "Premium", "Corporativo"],
+        )
+        Ctk.campo(self.fr_atributos, "-", columna=1, fila=0, state="disabled")
+        Ctk.separador_horizontal(frame_form)
+
+        # 2) SECCIÓN PARA DATOS PERSONALES (nombres, apellidos, rut)
+        fr_info = ttk.LabelFrame(
+            frame_form, text="Información Personal", padding=20
+        )
+        fr_info.pack()
+
+        self._campos["rut"] = Ctk.campo(
+            fr_info, "RUT", columna=0, fila=0, mb=True
+        )
+        self._campos["nombre"] = Ctk.campo(
+            fr_info, "Nombres", columna=1, fila=0, mb=True
+        )
+        self._campos["apellido_paterno"] = Ctk.campo(
+            fr_info, "Apellido Paterno", columna=0, fila=1
+        )
+        self._campos["apellido_materno"] = Ctk.campo(
+            fr_info, "Apellido Materno", 1, 1
+        )
+        Ctk.separador_horizontal(frame_form)
+
+        # 3) SECCIÓN PARA DATOS DE CONTACTO (correo, teléfono)
+        fr_contacto = ttk.LabelFrame(
+            frame_form, text="Datos Contacto", padding=20
+        )
+        fr_contacto.pack()
+
+        self._campos["correo"] = Ctk.campo(
+            fr_contacto, "Correo electrónico", columna=0, fila=0
+        )
+        self._campos["telefono"] = Ctk.campo(
+            fr_contacto, "Teléfono", columna=1, fila=0
+        )
+        Ctk.separador_horizontal(frame_form)
+
+        # 4) SECCIÓN PARA DIRECCIÓN (calle, número, región, comuna)
+        fr_dir = ttk.LabelFrame(frame_form, text="Dirección", padding=20)
+        fr_dir.pack()
+
+        self._campos["calle"] = Ctk.campo(
+            fr_dir, "Calle", columna=0, fila=0, mb=True
+        )
+        self._campos["numero"] = Ctk.campo(
+            fr_dir, "Número", columna=1, fila=0, mb=True
+        )
+        self._campos["region"] = Ctk.combo(
+            fr_dir, "Región", columna=0, fila=1, valores=regiones
+        )
+        self._campos["comuna"] = Ctk.combo(
+            fr_dir, "Comuna", columna=1, fila=1, valores=[]
+        )
+        Ctk.separador_horizontal(frame_form)
+
+        # 5) LABEL PARA MENSAJE DE ERROR (por dato erróneo en formulario)
+        self.label_estado = ttk.Label(frame_form, text="")
+        self.label_estado.pack()
+
+        # 6) SECCIÓN PARA BOTONES (crear/editar, cancelar)
+        fr_botones = ttk.Frame(frame_form)
+        fr_botones.pack(fill="x", expand=True)
+
+        ttk.Button(
+            fr_botones, text="Crear usuario", command=self._handle_guardar
+        ).pack(side="right")
+
+        ttk.Button(
+            fr_botones, text="Cancelar", command=self._handle_cancelar
+        ).pack(side="right", padx=10)
+
+        # ------------------------------ EVENTOS ------------------------------
+        # - actualizar opciones de comunas cuando se cambia la región
+        self._campos["region"].bind(
+            "<<ComboboxSelected>>", self._actualizar_comunas
+        )
+
+        # - cambiar entry (puntos, descuento, crédito) cuando se cambia el tipo de cliente
+        self._campos["tipo"].bind(
+            "<<ComboboxSelected>>", self._generar_campo_cliente
+        )
+
+        # - configuración del canvas para que el scroll funcione correctamente
+        frame_form.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            ),
+        )
+
+        # - configuración para que el scroll funcione con la rueda del mouse
+        self.canvas.bind_all("<MouseWheel>", self._handle_scroll)
+
+        # ----------------------------- FIN INIT ------------------------------
+
+    def _actualizar_comunas(self, _):
+        region: str = self._campos["region"].get()
         comunas = Direccion.obtener_comunas_por_region(region)
-        self.entry_comuna["values"] = comunas
-        self.entry_comuna.set("")
+        self._campos["comuna"]["values"] = comunas
+        self._campos["comuna"].set("")
 
-    def handle_crear_usuario(self):
-        entradas = {
-            "rut": self.entry_rut.get(),
-            "nombres": self.entry_nombres.get(),
-            "apellido_paterno": self.entry_paterno.get(),
-            "apellido_materno": self.entry_materno.get(),
-            "correo": self.entry_correo.get(),
-            "telefono": self.entry_telefono.get(),
-            "calle": self.entry_calle.get(),
-            "numero": self.entry_numero.get(),
-            "region": self.entry_region.get(),
-            "comuna": self.entry_comuna.get(),
-        }
+    def _leer_formulario(self) -> dict:
+        return {clave: widget.get() for clave, widget in self._campos.items()}
 
-        widgets = self.winfo_children()
-
-        for i in widgets:
-            print(isinstance(i, ttk.Entry))
+    def _handle_guardar(self):
+        formulario = self._leer_formulario()
+        print(formulario)
 
         # try:
         #     if any(e == "" for e in entradas.values()):
@@ -78,3 +168,30 @@ class VistaFormulario(ttk.Frame):
         #     print(respuesta)
         # except ValueError as e:
         #     self.label_estado.config(text=str(e))
+
+    def _handle_cancelar(self):
+        pass
+
+    def _handle_scroll(self, evento):
+        os = platform.system()
+        factor = evento.delta / 120 if os == "Windows" else evento.delta
+        self.canvas.yview_scroll(int(-1 * factor), "units")
+
+    def _generar_campo_cliente(self, _):
+        tipo = self._campos["tipo"].get()
+        tipos = {
+            "Regular": ("puntos", "Puntos disponibles"),
+            "Premium": ("porcentaje_descuento", "Porcentaje de descuento"),
+            "Corporativo": ("limite_credito", "Límite de crédito"),
+        }
+
+        # quitar propiedades extras de self._campos (dejar solo con campos comunes)
+        for clave in [c[0] for c in tipos.values()]:
+            self._campos.pop(clave, None)
+
+        # agregar propiedad a self._campos dependiendo del tipo de cliente
+        tipo = tipos.get(tipo, tipos["Regular"])
+        self._campos[tipo[0]] = Ctk.campo(
+            self.fr_atributos, tipo[1], columna=1, fila=0
+        )
+        print(self._campos.keys())
