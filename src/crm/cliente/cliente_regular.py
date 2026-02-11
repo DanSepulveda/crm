bd0b1ba39851580018f3c6e1b15891bd220112e2
Daniel Sepúlveda <dansepdev@gmail.com>
2026-02-11 08:25:34 -0300

    validations added

--- src/crm/cliente/cliente_regular.py
+++ src/crm/cliente/cliente_regular.py
@@ -12,40 +12,43 @@ class ClienteRegular(Cliente):
         return f"{super().__str__()} - Puntos: {self.puntos}"
 
     @staticmethod
-    def _validar_cantidad_positiva(valor: int, permitir_cero: bool = False):
-        if not isinstance(valor, int):
-            raise ValueError("Debe ingresar un número entero.")
+    def _validar_cantidad_positiva(
+        valor: int | str, nombre: str, permitir_cero: bool = False
+    ):
+        if (
+            isinstance(valor, str)
+            and not valor.isdigit()
+            and not isinstance(valor, int)
+        ):
+            raise ValueError(f"'{nombre}' debe ser un número entero.")
+
+        valor_numerico = int(valor)
 
         if permitir_cero:
-            if valor < 0:
-                raise ValueError("Debe ingresar un número positivo.")
+            if valor_numerico < 0:
+                raise ValueError(f"'{nombre}' debe ser mayor o igual que cero")
         else:
-            if valor <= 0:
-                raise ValueError("Debe ingresar un número mayor que cero.")
+            if valor_numerico <= 0:
+                raise ValueError(f"'{nombre}' debe ser mayor que cero.")
 
     @property
     def puntos(self) -> int:
         return self._puntos
 
     @puntos.setter
-    def puntos(self, cantidad: int):
-        self._validar_cantidad_positiva(cantidad, permitir_cero=True)
-        self._puntos = cantidad
+    def puntos(self, cantidad: int | str):
+        self._validar_cantidad_positiva(cantidad, "Puntos", permitir_cero=True)
+        self._puntos = int(cantidad)
 
-    def acumular_puntos(self, cantidad: int):
-        self._validar_cantidad_positiva(cantidad)
-        self._puntos += cantidad
-
-    def utilizar_puntos(self, cantidad: int):
-        self._validar_cantidad_positiva(cantidad)
-
-        if cantidad > self.puntos:
-            raise ValueError("Puntos insuficientes.")
-
-        self._puntos -= cantidad
-
-    def a_dict(self):
+    def a_diccionario(self) -> dict:
         """Retorna el objeto ClienteRegular en formato diccionario."""
-        diccionario = super().a_dict()
+        diccionario = super().a_diccionario()
         diccionario.update({"puntos": self.puntos})
         return diccionario
+
+    def acumular_por_compra(self, monto: int | str) -> str:
+        """Agrega puntos al Cliente dependiendo del monto de la compra."""
+        self._validar_cantidad_positiva(monto, "Monto", permitir_cero=False)
+        acumulados = int(monto) // 1000  # 1 punto cada $1.000
+        self._puntos += acumulados
+        return f"Ha acumulado {acumulados} puntos."
