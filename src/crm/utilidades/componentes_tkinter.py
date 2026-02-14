b07a42cadcaf9fc9fb199685195576374fe97fde
Daniel Sepúlveda <dansepdev@gmail.com>
2026-02-14 18:21:40 -0300

    fix: add width property to Entry

--- src/crm/utilidades/componentes_tkinter.py
+++ src/crm/utilidades/componentes_tkinter.py
@@ -3,14 +3,22 @@ from tkinter import ttk
 
 class ComponentesTkinter:
     @staticmethod
-    def campo(frame, label, columna, fila, mb: bool = False, **kwargs):
+    def campo(
+        frame,
+        label,
+        columna,
+        fila,
+        mb: bool = False,
+        width: int = 25,
+        **kwargs,
+    ):
         cframe = ttk.Frame(frame)
         cframe.grid(column=columna, row=fila)
 
         label_campo = ttk.Label(cframe, text=label, font=("Arial", 14, "bold"))
         label_campo.grid(column=0, row=0, padx=5, sticky="w")
 
-        entry = ttk.Entry(cframe, width=25, **kwargs)
+        entry = ttk.Entry(cframe, width=width, **kwargs)
         entry.grid(column=0, row=1, padx=5, pady=(0, 20) if mb else 0)
         return entry
 
